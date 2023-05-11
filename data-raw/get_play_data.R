0e5d95378894d0339fcbf05d96a6bb2a50fbd83c
Willem Klajbor <willem.klajbor@noaa.gov>
2023-05-11 16:51:35 -0600

    will's edits: - wrote code to batch in and reformat eiwg data - added region to summary table to do: - plot and summary table need to be subset by region - rewrite functions to pull from indicator_names list rather than folder

--- data-raw/get_play_data.R
+++ data-raw/get_play_data.R
@@ -3,23 +3,28 @@
 
 dataA <- data.frame(x = as.numeric(1:30),
                     y = as.numeric(0.1*c(1:30) + rnorm(10, sd = 0.35)),
-                    title = c("Apple"))
+                    title = c("Apple"),
+                    region = c("PNW"))
 
 dataB <- data.frame(x = 1:25,
                     y = -0.3*c(1:25) + rnorm(5, sd = 0.35),
-                    title = c("Banana"))
+                    title = c("Banana"),
+                    region = c("Island"))
 
 dataC <- data.frame(x = 1:30,
                     y = 1*c(1:30) + rnorm(10, sd = 0.35),
-                    title = c("Cucumber"))
+                    title = c("Cucumber"),
+                    region=c("SE"))
 
 dataD <- data.frame(x = 1:20,
                     y = 0.1*c(1:20) + rnorm(8, sd = 0.35),
-                    title = c("Dog"))
+                    title = c("Dog"),
+                    region = c("NE"))
 
 dataE <- data.frame(x = 1:35,
                     y = -0.7*c(1:35) + rnorm(6, sd = 0.35),
-                    title = c("Elephant"))
+                    title = c("Elephant"),
+                    region = c("Central"))
 
 write.csv(dataA, file = here::here("data-raw","dataA.csv"))
 
